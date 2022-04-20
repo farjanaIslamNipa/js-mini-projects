fdb9c8f88c420c78ba1a600b8dc187b071e0d39a
farjanaIslamNipa <nipa.farzana87@gmail.com>
2022-04-20 15:56:42 +0600

    countdown timer added

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ navToggle.addEventListener('click', function(){
 })
 
 
-// const colors = ['rgba(133, 122, 200)', '#f15025', '#79c2fa', '#79fae9', '#79fa8e', '#e7fa79', '#faad79', '#eb79fa', '#fa7979'];
 const colors = ['aqua', 'azure', 'bisque', 'brown', 'cadetblue', 'darkcyan', 'darkolivegreen', 'hotpink', 'mediumslateblue'];
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
@@ -287,6 +286,100 @@ tabs.addEventListener('click', function(e){
         element.classList.add('active');
     }
 })
+
+// countdown timer js
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  const weekdays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  const giveawsy = document.querySelector('.giveaway');
+  const deadline = document.querySelector('.deadline');
+  const items = document.querySelectorAll('.deadline-format h4');
+
+  let futureDate = new Date(2022,4,24,11,30,0);
+
+  const year = futureDate.getFullYear();
+  const hours = futureDate.getHours();
+  const mins = futureDate.getMinutes();
+
+  let month = futureDate.getMonth();
+  month = months[month];
+
+  const date = futureDate.getDate();
+
+  const weekday = weekdays[futureDate.getDay()];
+
+  giveawsy.textContent = `Giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${mins}am`;
+
+//   future time in ms
+const futureTime = futureDate.getTime();
+
+function getRemainingTime(){
+    const today = new Date().getTime();
+    const t = futureTime - today;
+
+    // 1s = 1000ms
+    // 1m = 60s
+    // 1hr = 60min
+    // 1d = 24hr
+
+    // values in ms
+    const oneDay = 24*60*60*1000;
+    const oneHour = 60*60*1000;
+    const oneMinute = 60*1000;
+    // calculate values
+    let days = t / oneDay;
+    days = Math.floor(days);
+    let hours = (t % oneDay) / oneHour;
+    hours = Math.floor(hours);
+    let minutes = Math.floor((t % oneHour) / oneMinute)
+    let seconds = Math.floor((t % oneMinute) / 1000);
+
+    // set values array
+
+    const values = [days, hours, minutes, seconds];
+    function format (item){
+        if(item<10){
+            return (item = `0${item}`);
+        }
+        return item
+    }
+    items.forEach(function(item, index){
+        item.innerHTML = format(values[index])
+    });
+    if(t<0){
+        clearInterval(countdown);
+        deadline.innerHTML = `<h4>Sorry, this giveaway has expired</h4>`
+    }
+}
+
+// countdown
+let countdown = setInterval(getRemainingTime, 1000);
+
+getRemainingTime();
+
+
+
 
 
 // Getting current date and time
