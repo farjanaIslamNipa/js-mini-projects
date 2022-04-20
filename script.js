e3ec3f23f4a19f56e0fb4e73fb0f452138288748
farjanaIslamNipa <nipa.farzana87@gmail.com>
2022-04-20 16:03:32 +0600

    updated counter

--- script.js
+++ script.js
@@ -316,7 +316,14 @@ const months = [
   const deadline = document.querySelector('.deadline');
   const items = document.querySelectorAll('.deadline-format h4');
 
-  let futureDate = new Date(2022,4,24,11,30,0);
+  let tempDate = new Date();
+
+  let tempYear = tempDate.getFullYear();
+  let tempMonth = tempDate.getMonth();
+  let tempDay = tempDate.getDate();
+
+//   let futureDate = new Date(2022,4,24,11,30,0);
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0)
 
   const year = futureDate.getFullYear();
   const hours = futureDate.getHours();
