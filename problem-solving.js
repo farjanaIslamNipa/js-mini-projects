bdff611dcd7686c5ce30c6917759640d7bfb2fab
farjana01 <nipa.farzana87@gmail.com>
2022-05-08 21:15:26 +0600

    solved rotating string

--- problem-solving.js
+++ problem-solving.js
@@ -23,3 +23,27 @@ function getCurrentTime(){
 
 currentDay.textContent = weekdays[today];
 currentTime.textContent = getCurrentTime();
+
+// Printing current page
+const printCurrentPage = () => {
+    window.print();
+}
+
+// Rotating string
+function animate_string(id){
+    const element = document.getElementById(id);
+    const textNode = element.childNodes[0];
+    let text = textNode.data;
+
+    setInterval(function(){
+        text = text[text.length - 1] + text.substring(0, text.length -1);
+        textNode.data = text;
+    }, 100)
+}
+
+
+
+
+
+
+
