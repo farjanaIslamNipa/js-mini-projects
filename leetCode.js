1c5fc5853c41f714639569eb3f5c59452a56d1fc
farjanaIslamNipa <nipa.farzana87@gmail.com>
2024-05-04 18:03:14 +0600

    leetcode problem solving practice

--- leetCode.js
+++ leetCode.js
@@ -11,9 +11,34 @@ var createCounter = function(init) {
 
 const counter = createCounter(5)
 
-console.log(counter.increment())
-console.log(counter.increment())
-console.log(counter.increment())
-console.log(counter.decrement())
-console.log(counter.reset())
-console.log(counter.decrement())
+// console.log(counter.increment())
+// console.log(counter.increment())
+// console.log(counter.increment())
+// console.log(counter.decrement())
+// console.log(counter.reset())
+// console.log(counter.decrement())
+
+const double = (nm, i) => {
+  if (nm > 8){
+    return nm * i
+  }
+}
+
+const fruits = [10, 8, 5]
+
+const map = function (arr, fn){
+  let filteredArr = []
+
+  arr.map((el, i) => {
+    if(Boolean(el)){
+      return filteredArr.push(fn(el, i))
+    }
+  })
+
+  // arr.forEach((el, index) => res.push(fn(el, index)))
+
+return filteredArr;
+}
+
+console.log(map(fruits, double))
+
