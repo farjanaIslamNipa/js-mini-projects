d74c072950e4092ab621099699069205e67ac546
farjanaIslamNipa <nipa.farzana87@gmail.com>
2025-05-17 18:27:43 +0600

    Create a 10 second timer

--- projects/Js-mastery/js-snippets.js
+++ projects/Js-mastery/js-snippets.js
@@ -1,5 +1,27 @@
 function getCurrentDate(){
-  return (new Date().toLocaleString())
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true
+  }
+  return (new Date().toLocaleString('en-US', options))
 }
 
-console.log(getCurrentDate(), 'date');
+// ###### Find the maximum number in an array
+const arr = [5, 55, 7, 18, 20]
+function findMax(arr){
+  return Math.max(...arr)
+}
+
+let seconds = 10
+const countdown = setInterval(() => {
+  seconds--
+  if(seconds < 0){
+    clearInterval(countdown)
+    console.log('count down finished');
+  }
+}, 1000)
